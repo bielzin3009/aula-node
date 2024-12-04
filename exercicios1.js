4d360b0c08383e65e415e676132a5ccdf450e07f
Biel <gabrielbp3009@gmail.com>
2024-12-03 21:49:15 -0300

    tentando aprender essa bomba

--- exercicios1.js
+++ exercicios1.js
@@ -3,3 +3,12 @@
 let texto = "Oi Gabriel"
 let textoinvertido = texto.split("").reverse().join("")
 console.log(textoinvertido)
+
+// contar vogais e consoantes
+// texto -> vogais = 2; consoantes = 3
+let nome = "Gabriel"
+let vogais = ["a", "e", "i", "o", "u"]
+
+if(nome.includes(vogais)) {
+    return
+}
